e2f457565a32bbdd87c91d400defb314dda8e385
satori-01 <132801948+satori-01@users.noreply.github.com>
2023-08-02 00:24:09 +0600

    Add files via upload

---
+++ dz7/script.js
@@ -0,0 +1,14 @@
+const answerButtons = document.querySelectorAll('.answer-button');
+
+answerButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const answer = button.nextElementSibling;
+    if (answer.style.display === 'none') {
+      answer.style.display = 'block';
+      button.textContent = 'Скрыть ответ';
+    } else {
+      answer.style.display = 'none';
+      button.textContent = 'Показать ответ';
+    }
+  });
+});
